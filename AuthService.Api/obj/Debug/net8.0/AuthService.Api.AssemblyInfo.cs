e7e36053b42f80c6fc56371afebfe5415f85bf45
ClaudioPina <claudio.pina.cartes@gmail.com>
2026-04-01 11:39:09 -0300

    01042026 Agrega cambios para despliegue de BD en Fly.io

--- AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
+++ AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82553ebd95256cd189cd76882b65c334f5f8eeb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad533682200efd5888a6e702f99cd647d8eead5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
